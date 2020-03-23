32b6c55805d5bbe64b8602ad3246d3f7d67227a9
Bas Straathof <bas_straathof@msn.com>
2020-03-23 17:28:24 +0100

    Validate events based on intime and outtime

--- validate_events.py
+++ validate_events.py
@@ -4,6 +4,8 @@ import shutil
 
 from sys import argv
 from tqdm import tqdm
+from datetime import datetime, timedelta
+
 import pandas as pd
 
 
@@ -19,20 +21,45 @@ def parse_cl_args():
 
 
 def validate_events(df_admission, df_events, stats):
-    if list(stats.keys()) != ['tot_nb_events', 'no_hadm_id_and_icustay_id',
+    if list(stats.keys()) != ['tot_nb_events', 'no_charttime',
+            'incorrect_charttime', 'no_hadm_id_and_icustay_id',
             'incorrect_hadm_id', 'incorrect_icustay_id', 'final_nb_events']:
-        raise ValueError('The keys of stays must be: tot_nb_events, ' +
+        raise ValueError('The keys of stats must be: tot_nb_events, ' +
+                'no_charttime, incorrect_charttime, ' +
                 'no_hadm_id_and_icustay_id, incorrect_hadm_id, ' +
                 'incorrect_icustay_id, final_nb_events')
+    tot_events = len(df_events)
 
+    # Make sure that the charttime field is datatime
+    df_events.CHARTTIME = pd.to_datetime(df_events.CHARTTIME)
+
+    # Obtain the HADM_ID and ICUSTAY_ID from df_admission
     hadm_id = df_admission.HADM_ID.tolist()[0]
     icustay_id = df_admission.ICUSTAY_ID.tolist()[0]
-    tot_events = len(df_events)
+
+    # Obtain INTIME and OUTTIME from df_admission and round down to hour
+    date_format = "%Y-%m-%d %H:%M:%S"
+    intime = df_admission.INTIME.tolist()[0]
+    intime = datetime.strptime(intime, date_format)
+    intime = intime.replace(microsecond=0, second=0, minute=0)
+    outtime = df_admission.OUTTIME.tolist()[0]
+    outtime = datetime.strptime(outtime, date_format)
+    outtime = outtime.replace(microsecond=0, second=0, minute=0)
+
+    # Drop events without a charttime
+    df_events = df_events[df_events.CHARTTIME.notna()]
+    no_charttime = tot_events - len(df_events)
+
+    # Drop events that fall outside the intime-outtime interval
+    mask = (intime <= df_events.CHARTTIME) & (df_events.CHARTTIME < outtime)
+    df_events = df_events.loc[mask]
+    incorrect_charttime = tot_events - len(df_events) - no_charttime
 
     # Drop events for which both HADM_ID and ICUSTAY_ID are empty
     df_events = df_events.dropna(how='all', subset=['HADM_ID',
         'ICUSTAY_ID'])
-    no_hadm_and_icustay = tot_events - len(df_events)
+    no_hadm_and_icustay = tot_events - len(df_events) - no_charttime \
+            - incorrect_charttime
 
     # Keep events without HADM_ID but with correct ICUSTAY_ID as well as
     # events without ICUSTAY_ID but with correct HADM_ID
@@ -41,14 +68,17 @@ def validate_events(df_admission, df_events, stats):
 
     # Drop events with wrong HADM_ID
     df_events = df_events.loc[df_events.HADM_ID == hadm_id]
-    wrong_hadm = tot_events - len(df_events) - no_hadm_and_icustay
+    wrong_hadm = tot_events - len(df_events) - no_charttime \
+            - incorrect_charttime - no_hadm_and_icustay
 
     # Drop events with wrong ICUSTAY_ID
     df_events = df_events.loc[df_events.ICUSTAY_ID == icustay_id]
-    wrong_icustay = tot_events - len(df_events) - no_hadm_and_icustay - \
-            wrong_hadm
+    wrong_icustay = tot_events - len(df_events) - no_charttime \
+            - incorrect_charttime - no_hadm_and_icustay - wrong_hadm
 
     stats['tot_nb_events'] += tot_events
+    stats['no_charttime'] += no_charttime
+    stats['incorrect_charttime'] += incorrect_charttime
     stats['no_hadm_id_and_icustay_id'] += no_hadm_and_icustay
     stats['incorrect_hadm_id'] += wrong_hadm
     stats['incorrect_icustay_id'] += wrong_icustay
@@ -62,10 +92,12 @@ def main(args):
     subject_directories = os.listdir(subjects_path)
     subject_directories = set(filter(lambda x: str.isdigit(x),
         subject_directories))
+    tot_subjects = len(subject_directories)
+    removed_subjects = 0
 
-    stats = {'tot_nb_events': 0, 'no_hadm_id_and_icustay_id': 0,
-            'incorrect_hadm_id': 0, 'incorrect_icustay_id': 0,
-            'final_nb_events': 0}
+    stats = {'tot_nb_events': 0, 'no_charttime': 0, 'incorrect_charttime': 0,
+            'no_hadm_id_and_icustay_id': 0, 'incorrect_hadm_id': 0,
+            'incorrect_icustay_id': 0, 'final_nb_events': 0}
 
     for i, sd in enumerate(tqdm(subject_directories)):
         # Read the admission for current subject
@@ -97,6 +129,9 @@ def main(args):
     if verbose:
         for k, v in stats.items():
             print(k+':', v)
+        print(f'From the initial {tot_subjects} subjects, ' \
+                f'{tot_subjects-removed_subjects} remain that have ' \
+                'events associated with them.')
 
 
 if __name__ == '__main__':
