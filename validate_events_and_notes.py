1764a1d0bf3e89738663bf107a0700f9efc47575
Bas Straathof <bas_straathof@msn.com>
2020-03-31 20:47:56 +0200

    Improve event + notes validation script

--- validate_events_and_notes.py
+++ validate_events_and_notes.py
@@ -1,6 +1,14 @@
-import argparse
-import os
-import shutil
+#!/usr/bin/python3
+
+"""validate_events_and_notes.py
+
+Script to validate the clinical events and notes extracted with
+extract_event_data_per_subject.py.
+"""
+
+__author__ = "Bas Straathof"
+
+import argparse, csv, os, shutil
 
 from sys import argv
 from tqdm import tqdm
@@ -8,31 +16,36 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 
+
 def parse_cl_args():
     """Parses CL arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument('-sp', '--subjects-path', type=str, default='data/',
             help='Path to subject directories.')
+    parser.add_argument('-op', '--output-path', type=str,
+            default='validation_statistics.csv',
+            help='Path to the output file.')
     parser.add_argument('-v', '--verbose', type=int,
             help='Level of verbosity in console output.', default=1)
 
     return parser.parse_args(argv[1:])
 
 
-def validate_events_and_notes(df_admission, df_events, df_notes, stats_events,
-        stats_notes):
-    if list(stats_events.keys()) != ['tot_nb_events', 'no_value',
-            'no_charttime', 'incorrect_charttime', 'no_hadm_id_and_icustay_id',
-            'incorrect_hadm_id', 'incorrect_icustay_id', 'final_nb_events']:
-        raise ValueError('The keys of stats must be: tot_nb_events, ' +
-                'no_value, no_charttime, incorrect_charttime, ' +
-                'no_hadm_id_and_icustay_id, incorrect_hadm_id, ' +
-                'incorrect_icustay_id, final_nb_events')
-
-    if list(stats_notes.keys()) != ['tot_nb_notes', 'no_text',
-            'incorrect_charttime', 'incorrect_hadm_id', 'final_nb_notes']:
-        raise ValueError('The keys of stats must be: tot_nb_notes, note_text, ' +
-                'incorrect_charttime, incorrect_hadm_id, final_nb_notes')
+def validate_events_and_notes(df_stay, df_events, df_notes, stats):
+    if list(stats.keys()) != ['events_tot_nb_events', 'events_no_value',
+            'events_no_charttime', 'events_incorrect_charttime',
+            'events_no_hadm_id_and_icustay_id', 'events_incorrect_hadm_id',
+            'events_incorrect_icustay_id', 'events_final_nb_events',
+            'notes_tot_nb_notes', 'notes_no_text', 'notes_incorrect_charttime',
+            'notes_incorrect_hadm_id', 'notes_final_nb_notes']:
+        raise ValueError('The keys of stats must be: events_tot_nb_events, ' +
+                'events_no_value, events_no_charttime, ' +
+                'events_incorrect_charttime, ' +
+                'events_no_hadm_id_and_icustay_id, events_incorrect_hadm_id, ' +
+                'events_incorrect_icustay_id, events_final_nb_events, ' +
+                'notes_tot_nb_notes, notes_note_text, ' +
+                'notes_incorrect_charttime, notes_incorrect_hadm_id, ' +
+                'notes_final_nb_notes')
 
     tot_events, tot_notes = len(df_events), len(df_notes)
 
@@ -40,16 +53,16 @@ def validate_events_and_notes(df_admission, df_events, df_notes, stats_events,
     df_events.CHARTTIME = pd.to_datetime(df_events.CHARTTIME)
     df_notes.CHARTTIME = pd.to_datetime(df_notes.CHARTTIME)
 
-    # Obtain the HADM_ID and ICUSTAY_ID from df_admission
-    hadm_id = df_admission.HADM_ID.tolist()[0]
-    icustay_id = df_admission.ICUSTAY_ID.tolist()[0]
+    # Obtain the HADM_ID and ICUSTAY_ID from df_stay
+    hadm_id = df_stay.HADM_ID.tolist()[0]
+    icustay_id = df_stay.ICUSTAY_ID.tolist()[0]
 
-    # Obtain INTIME and OUTTIME from df_admission and round down to hour
+    # Obtain INTIME and OUTTIME from df_stay and round down to hour
     date_format = "%Y-%m-%d %H:%M:%S"
-    intime = df_admission.INTIME.tolist()[0]
+    intime = df_stay.INTIME.tolist()[0]
     intime = datetime.strptime(intime, date_format)
     intime = intime.replace(microsecond=0, second=0, minute=0)
-    outtime = df_admission.OUTTIME.tolist()[0]
+    outtime = df_stay.OUTTIME.tolist()[0]
     outtime = datetime.strptime(outtime, date_format)
     outtime = outtime.replace(microsecond=0, second=0, minute=0)
 
@@ -110,50 +123,48 @@ def validate_events_and_notes(df_admission, df_events, df_notes, stats_events,
     incorrect_hadm_id = tot_notes - len(df_notes) - no_text - \
             incorrect_charttime
 
-    stats_events['tot_nb_events'] += tot_events
-    stats_events['no_value'] += no_value
-    stats_events['no_charttime'] += no_charttime
-    stats_events['incorrect_charttime'] += incorrect_charttime
-    stats_events['no_hadm_id_and_icustay_id'] += no_hadm_and_icustay
-    stats_events['incorrect_hadm_id'] += wrong_hadm
-    stats_events['incorrect_icustay_id'] += wrong_icustay
-    stats_events['final_nb_events'] += len(df_events)
+    stats['events_tot_nb_events'] += tot_events
+    stats['events_no_value'] += no_value
+    stats['events_no_charttime'] += no_charttime
+    stats['events_incorrect_charttime'] += incorrect_charttime
+    stats['events_no_hadm_id_and_icustay_id'] += no_hadm_and_icustay
+    stats['events_incorrect_hadm_id'] += wrong_hadm
+    stats['events_incorrect_icustay_id'] += wrong_icustay
+    stats['events_final_nb_events'] += len(df_events)
+    stats['notes_tot_nb_notes'] += tot_notes
+    stats['notes_no_text'] += no_text
+    stats['notes_incorrect_charttime'] += incorrect_charttime
+    stats['notes_incorrect_hadm_id'] += incorrect_hadm_id
+    stats['notes_final_nb_notes'] += len(df_notes)
 
-    stats_notes['tot_nb_notes'] += tot_notes
-    stats_notes['no_text'] += no_text
-    stats_notes['incorrect_charttime'] += incorrect_charttime
-    stats_notes['incorrect_hadm_id'] += incorrect_hadm_id
-    stats_notes['final_nb_notes'] += len(df_notes)
-
-    return df_events, df_notes, stats_events, stats_notes
+    return df_events, df_notes, stats
 
 
 def main(args):
-    verbose, subjects_path = args.verbose, args.subjects_path
+    subjects_path = args.subjects_path
     subject_directories = os.listdir(subjects_path)
     subject_directories = set(filter(lambda x: str.isdigit(x),
         subject_directories))
     tot_subjects = len(subject_directories)
     removed_subjects = 0
 
-    stats_events = {'tot_nb_events': 0, 'no_value': 0, 'no_charttime': 0,
-            'incorrect_charttime': 0, 'no_hadm_id_and_icustay_id': 0,
-            'incorrect_hadm_id': 0, 'incorrect_icustay_id': 0,
-            'final_nb_events': 0}
-
-    stats_notes = {'tot_nb_notes': 0, 'no_text': 0, 'incorrect_charttime': 0,
-            'incorrect_hadm_id': 0, 'final_nb_notes': 0}
-
+    stats = {'events_tot_nb_events': 0, 'events_no_value': 0,
+            'events_no_charttime': 0, 'events_incorrect_charttime': 0,
+            'events_no_hadm_id_and_icustay_id': 0,
+            'events_incorrect_hadm_id': 0, 'events_incorrect_icustay_id': 0,
+            'events_final_nb_events': 0, 'notes_tot_nb_notes': 0,
+            'notes_no_text': 0, 'notes_incorrect_charttime': 0,
+            'notes_incorrect_hadm_id': 0, 'notes_final_nb_notes': 0}
 
     for i, sd in enumerate(tqdm(subject_directories)):
-        # Read the admission for current subject
-        df_admission = pd.read_csv(os.path.join(subjects_path, sd,
-            'admission.csv'))
+        # Read the stay data for current subject
+        df_stay = pd.read_csv(os.path.join(subjects_path, sd,
+            'stay.csv'))
 
         # Assert the data frame only contains valid ICUSTAY_ID and HADM_ID
-        assert(not df_admission.ICUSTAY_ID.isnull().any() and
-                not df_admission.HADM_ID.isnull().any() and
-                len(df_admission == 1))
+        assert(not df_stay.ICUSTAY_ID.isnull().any() and
+                not df_stay.HADM_ID.isnull().any() and
+                len(df_stay == 1))
 
         # Read the events for the current subject
         df_events = pd.read_csv(os.path.join(subjects_path, sd, 'events.csv'))
@@ -162,9 +173,8 @@ def main(args):
         df_notes = pd.read_csv(os.path.join(subjects_path, sd, 'notes.csv'))
 
         # Validate events and notes
-        df_events, df_notes, stats_events, stats_notes = \
-                validate_events_and_notes(df_admission, df_events, df_notes,
-                stats_events, stats_notes)
+        df_events, df_notes, stats = validate_events_and_notes(df_stay,
+                df_events, df_notes, stats)
 
         if (not df_events.empty) and (not df_notes.empty) :
             # Write df_events to events.csv
@@ -182,10 +192,14 @@ def main(args):
             except OSError as e:
                 print (f'Error: {e.filename} - {e.strerror}.')
 
-    if verbose:
-        for k, v in stats_events.items():
-            print(k+':', v)
-        for k, v in stats_notes.items():
+    # Write results to the file
+    with open(args.output_path, 'w') as wf:
+        writer = csv.writer(wf)
+        for key, value in stats.items():
+            writer.writerow([key, value])
+
+    if args.verbose:
+        for k, v in stats.items():
             print(k+':', v)
         print(f'From the initial {tot_subjects} subjects, ' \
                 f'{tot_subjects-removed_subjects} remain that have ' \
@@ -194,3 +208,4 @@ def main(args):
 
 if __name__ == '__main__':
     main(parse_cl_args())
+
