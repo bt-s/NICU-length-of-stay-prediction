4990f08da10c86c130cc759cb4d9a7b0cfe08522
Bas Straathof <bas_straathof@msn.com>
2020-04-01 17:50:21 +0200

    Refactor code to preprocess events per subject

---
+++ preprocess_events_per_subjects.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python3
+
+"""preprocess_events_per_subject.py
+
+Script to preprocess the clinical events:
+    - Filter selected variables
+    - Clean variables
+    - Remove invalid values
+"""
+
+__author__ = "Bas Straathof"
+
+import argparse, csv, json, os, sys
+
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+
+from tqdm import tqdm
+
+from preprocessing_utils import clean_variables, remove_invalid_values
+from utils import remove_subject_dir
+
+
+def parse_cl_args():
+    """Parses CL arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sp', '--subjects-path', type=str, default='data/',
+            help='Path to subject directories.')
+    parser.add_argument('-op', '--output-path', type=str,
+            default='variable_statistics.csv', help='Path to the output file.')
+    parser.add_argument('-cp', '--config-path', type=str, default='config.json',
+            help='Path to the JSON configuration file.')
+    parser.add_argument('-v', '--verbose', type=int,
+            help='Level of verbosity in console output.', default=1)
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def main(args):
+    subjects_path, verbose = args.subjects_path, args.verbose
+
+    with open(args.config_path) as f:
+        config = json.load(f)
+        vars_to_itemid = config['vars_to_itemid']
+        valid_ranges = config['valid_variable_ranges']
+
+    subject_directories = os.listdir(subjects_path)
+    subject_directories = set(filter(lambda x: str.isdigit(x),
+        subject_directories))
+
+
+    if verbose: print("Filtering and cleaning selected variables...")
+    tot_subjects = len(subject_directories)
+    removed_subjects, tot_events, tot_events_kept = 0, 0, 0
+
+    # Create item_id to var dictionary based on vars_to_itemid
+    item_id_to_vars = {}
+    for var, item_ids in vars_to_itemid.items():
+        for item_id in item_ids:
+            item_id_to_vars[item_id] = var
+
+    # Create a list of variables to keep
+    itemids_to_keep = list(item_id_to_vars.keys())
+
+    # Create a pandas dataframe based on item_id_to_vars
+    df_item_id  = pd.DataFrame(item_id_to_vars.items(),
+            columns=['ITEMID', 'VARIABLE'])
+
+    # Initialize variable counts dictionary
+    variable_counts = {}
+    for var in vars_to_itemid.keys():
+        variable_counts[var] = {'VALUES': [], 'SUBJECTS': 0,
+                'INVALID_VALUES': 0}
+
+    for i, subject_dir in enumerate(tqdm(subject_directories)):
+        # Read the events dataframe
+        df_events = pd.read_csv(os.path.join(subjects_path, str(subject_dir),
+            'events.csv'))
+
+        tot_events += len(df_events)
+
+        # Filter the dataframe
+        df_events = pd.merge(df_events, df_item_id, how='inner', on='ITEMID')
+        df_events = df_events[df_events.VALUE.notnull()]
+
+        # Clean variables
+        df_events = clean_variables(df_events)
+
+        # Clean charttime -- we know from the format that the length should
+        # always be 19
+        df_events = df_events[df_events.CHARTTIME.str.len() == 19]
+
+        # Remove invalid values
+        df_events, variable_counts = remove_invalid_values(df_events,
+                valid_ranges, variable_counts)
+
+        # Sort on CHARTTIME
+        df_events = df_events.sort_values(by='CHARTTIME')
+
+        tot_events_kept += len(df_events)
+
+        # Write df_events to CSV
+        if not df_events.empty:
+            df_events.to_csv(os.path.join(subjects_path, str(subject_dir),
+                'events.csv'), index=False)
+        else:
+            remove_subject_dir(os.path.join(subjects_path, str(subject_dir)))
+            removed_subjects += 1
+
+    # Write results to the file
+    with open(args.output_path, 'w') as wf:
+        csv_header = ['VARIABLE', 'COUNT', 'SUBJECTS', 'INVALID_VALUES', 'MIN',
+                'MAX', 'MEAN', 'MEDIAN']
+
+        wf.write(','.join(csv_header) + '\n')
+        csv_writer = csv.DictWriter(wf, fieldnames=csv_header,
+            quoting=csv.QUOTE_MINIMAL)
+
+        for key, val in variable_counts.items():
+            results = {
+                'VARIABLE': key,
+                'COUNT': len(variable_counts[key]['VALUES']),
+                'SUBJECTS': variable_counts[key]['SUBJECTS'],
+                'INVALID_VALUES': variable_counts[key]['INVALID_VALUES'],
+                'MIN': np.min(variable_counts[key]['VALUES']),
+                'MAX': np.max(variable_counts[key]['VALUES']),
+                'MEAN': np.mean(variable_counts[key]['VALUES']),
+                'MEDIAN': np.median(variable_counts[key]['VALUES'])}
+
+            csv_writer.writerows([results])
+
+    if verbose:
+        print(f'Of the initial {tot_subjects} subjects, ' \
+                f'{tot_subjects-removed_subjects} remain that have valid ' \
+                f'variables of interest associated with them.\nOf the ' \
+                f'initial {tot_events} events, {tot_events_kept} remain ' \
+                f'which are variables of interest.')
+
+        total_invalid_values = 0
+        for key, val in variable_counts.items():
+            total_invalid_values += variable_counts[key]['INVALID_VALUES']
+        print(f'The total number of invalid values is: {total_invalid_values}')
+
+
+if __name__ == '__main__':
+    main(parse_cl_args())
+
