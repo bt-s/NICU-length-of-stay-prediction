6f0ab16f997d41eec6b9c23b672a5d12094873b4
Bas Straathof <bas_straathof@msn.com>
2020-04-01 17:43:13 +0200

    Refactor tests for set_targets function

--- test_set_targets.py
+++ test_set_targets.py
@@ -1,23 +1,24 @@
-from utils import set_targets
+from utils import los_hours_to_target
 import unittest
 import pandas as pd
 
 
 class TestTargetSetter(unittest.TestCase):
     def setUp(self):
-        self.test_df = pd.DataFrame(data={'LOS': [0, 0.2, 1, 1.2, 2, 2.2, 3,
-            3.2, 4, 4.2, 5, 5.2, 6, 6.2, 7, 7.2, 8, 8.2, 9, 13.2, 14, 14.2]})
-
-        self.test_df_w_targets = set_targets(self.test_df)
+        self.test_hours = [0, 5, 24, 29, 48, 53, 72, 77, 96, 101, 120, 125, 144,
+                149, 168, 173, 192, 197, 216, 317, 336, 341]
 
         self.expected_targets = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7,
                 7, 8, 8, 8, 8, 9, 9]
 
     def test_set_targets(self):
-        for exp_t, (ix, row) in zip(self.expected_targets,
-                self.test_df_w_targets.iterrows()):
-            t = int(self.test_df_w_targets.iloc[ix]['TARGET'])
-            self.assertEqual(t, exp_t)
+        test_targets = []
+        for hour in self.test_hours:
+            test_targets.append(los_hours_to_target(hour))
+
+        for exp_t, test_t in zip(self.expected_targets,
+                test_targets):
+            self.assertEqual(exp_t, test_t)
 
 
 if __name__ == '__main__':
