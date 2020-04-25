7b55bb1bd780c9638bc79b8006ac76b5229358f2
bt-s <bas_straathof@msn.com>
2020-04-25 19:40:37 +0200

    Parallelize and improve hyper-parameter search for logistic regression

--- nicu_los/src/modelling/logistic_regression.py
+++ nicu_los/src/modelling/logistic_regression.py
@@ -15,6 +15,8 @@ from sys import argv
 from datetime import datetime
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.metrics import make_scorer, cohen_kappa_score
 
 from ..utils.evaluation_utils import evaluate_classification_model
 from ..utils.modelling_utils import get_train_val_test_baseline_sets
@@ -43,7 +45,6 @@ def main(args):
     if not os.path.exists(args.models_path):
         os.makedirs(args.models_path)
 
-    v_print = print if args.verbose else lambda *a, **k: None
     data_path = args.baseline_data_path
     save_model = args.save_model
     now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
@@ -52,63 +53,67 @@ def main(args):
     X_train, y_train, X_val, y_val, X_test, y_test = \
             get_train_val_test_baseline_sets(data_path, task='classification')
 
+    # The training and validation set need to be fed conjointly to GridSearchCV
+    X = np.vstack((X_train, X_val))
+    y = np.hstack((y_train, y_val))
+
+    # Define what indices of X belong to x_train, and which to x_Val
+    val_idx = np.hstack((np.ones(X_train.shape[0])*-1, np.ones(X_val.shape[0])))
+    ps = PredefinedSplit(test_fold=val_idx)
+
+    # Initialize the logistic regression hyper-paramters
+    LR = LogisticRegression(random_state=42, multi_class="multinomial",
+            solver='saga')
+
+    # Define the parameter grid
     if args.grid_search:
-        regularizers = ['l1', 'l1', 'l1', 'l1', 'l1', 'l2', 'l2', 'l2', 'l2',
-                'l2']
-        Cs = [1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 1.0, 0.1, 0.01, 0.001,
-                0.0001, 0.00001]
+        regularizers = ['l1', 'l2']
+        Cs = [1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001 ]
     else:
         regularizers = ['l2']
         Cs = [1.0]
 
-    best_model, best_kappa = None, 0
-    for (regularizer, C) in zip(regularizers, Cs):
-        v_print(f'Fitting LR model with penalty={regularizer} and C={C}')
-        # Create Numpy arrays for storing the activations
-        train_act = np.zeros(shape=y_train.shape, dtype=float)
-        val_act = np.zeros(shape=y_val.shape, dtype=float)
-        test_act = np.zeros(shape=y_test.shape, dtype=float)
+    param_grid = dict(C=Cs, penalty=regularizers)
 
-        clf = LogisticRegression(random_state=42, multi_class="multinomial",
-                penalty=regularizer, C=C, solver='saga', n_jobs=-1)
+    # Initialize the grid serach using the predefined train-validation split
+    clf = GridSearchCV(LR, param_grid=param_grid, n_jobs=-1, cv=ps,
+    scoring=make_scorer(cohen_kappa_score), verbose=3)
 
-        # Fit the model
-        clf.fit(X_train, y_train)
+    # Fit the GridSearchCV to find the optimal estimator
+    clf.fit(X, y)
 
-        # Predict on the validation set
-        val_preds = clf.predict_proba(X_val)
-        val_act[:] = np.argmax(val_preds, axis=1)
+    # Extract the best estimator and fit again on all available training data
+    best_clf = clf.best_estimator_
+    best_clf.fit(X, y)
 
-        # Predict on the testing set
-        test_preds = clf.predict_proba(X_test)
-        test_act[:] = np.argmax(test_preds, axis=1)
+    # Predict on the training set
+    train_preds = clf.predict_proba(X)
+    train_act[:] = np.argmax(train_preds, axis=1)
 
-        # Evaluate the model on the validation set
-        val_scores = evaluate_classification_model(y_val, val_act)
+    # Predict on the testing set
+    test_preds = clf.predict_proba(X_test)
+    test_act[:] = np.argmax(test_preds, axis=1)
 
-        # Only keep the best model
-        if val_scores['kappa'] > best_kappa:
-            best_kappa = val_scores['kappa']
-            best_model = clf
+    # Evaluate the model on the training set
+    train_scores = evaluate_classification_model(y, train_act)
 
-        # Evaluate the model on the test set
-        test_scores = evaluate_classification_model(y_test, test_act)
-
-        if save_model:
-            f_name = os.path.join(args.models_path, f'results_{now}.txt')
-
-            with open(f_name, "a") as f:
-                f.write(f'LR: penalty={regularizer}, C={C}:\n')
-                f.write(f'- Validation scores:\n')
-                for k, v in val_scores.items():
-                    f.write(f'\t\t{k}: {v}\n')
-                f.write(f'- Test scores:\n')
-                for k, v in test_scores.items():
-                    f.write(f'\t\t{k}: {v}\n')
+    # Evaluate the model on the test set
+    test_scores = evaluate_classification_model(y_test, test_act)
 
     if save_model:
+        f_name = os.path.join(args.models_path, f'results_{now}.txt')
+
+        with open(f_name, "a") as f:
+            f.write(f'Best LR model: {clf.best_estimator_}:\n')
+            f.write(f'- Training scores:\n')
+            for k, v in train_scores.items():
+                f.write(f'\t\t{k}: {v}\n')
+            f.write(f'- Test scores:\n')
+            for k, v in test_scores.items():
+                f.write(f'\t\t{k}: {v}\n')
+
         f_name = os.path.join(args.models_path,
-                f'model_{now}_{regularizer}_{C}_kappa_{best_kappa}.pkl')
+                f'best_model_{now}.pkl')
 
         with open(f_name, 'wb') as f:
             pickle.dump(clf, f)
