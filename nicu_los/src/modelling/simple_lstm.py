853906d79b364606ca405d67e1350cbddf3476eb
bt-s <bas_straathof@msn.com>
2020-04-30 21:05:41 +0200

    First try at training a simple LSTM

---
+++ nicu_los/src/modelling/simple_lstm.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+
+"""simple_lstm.py
+
+Implementation of a simple LSTM model to predict the remaining length-of-stay.
+"""
+
+__author__ = "Bas Straathof"
+
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import CSVLogger, EarlyStopping, \
+        ModelCheckpoint, TensorBoard
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+
+import argparse, json, os
+from sys import argv
+from datetime import datetime
+
+from ..utils.modelling_utils import data_generator, get_bucket_by_seq_len, \
+        construct_simple_lstm
+from ..utils.utils import get_subject_dirs
+
+
+def parse_cl_args():
+    """Parses CL arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', type=str, default='data/',
+            help='Path to the data directories.')
+    parser.add_argument( '--models-path', type=str,
+            default='models/simple_lstm',
+            help='Path to the simple LSTM models directory.')
+    parser.add_argument('--batch-size', type=int, default=8, help='Batch size.')
+    parser.add_argument('--mask-indicator', type=bool, default=True,
+            help='Whether to use missinggness indicator mask variables.')
+    parser.add_argument('--steps', type=int, default=20,
+            help='Steps per epoch.')
+    parser.add_argument('--epochs', type=int, default=20,
+            help='Epochs.')
+    parser.add_argument('--training', type=bool, default=True,
+            help='Whether the current phase is the training phase.')
+    parser.add_argument('--checkpoint-file', type=str, default="",
+            help='File from which to load the model weights.')
+    parser.add_argument('--initial-epoch', type=int, default=0,
+            help='The starting epoch if loading a checkpoint file.')
+
+    return parser.parse_args(argv[1:])
+
+
+def main(args):
+    models_path = args.models_path
+    if not os.path.exists(models_path):
+        os.makedirs(models_path)
+
+    checkpoints_dir = os.path.join(models_path, 'checkpoints')
+    if not os.path.exists(checkpoints_dir):
+        os.makedirs(checkpoints_dir)
+
+    log_dir = models_path + '/logs/' + datetime.now().strftime('%Y%m%d-%H%M%S')
+
+    mask = args.mask_indicator
+    batch_size = args.batch_size
+    steps = args.steps
+    initial_epoch = args.initial_epoch
+    training = args.training
+    verbose = args.verbose
+
+    checkpoint_path = os.path.join(checkpoints_dir,
+            f'batch{batch_size}-steps{steps}-epoch' + \
+            '{epoch:02d}.h5')
+
+    bucket_by_seq_len = get_bucket_by_seq_len(batch_size)
+
+    # Obtain the training variables
+    with open('nicu_los/config.json') as f:
+        config = json.load(f)
+        variables = config['variables']
+
+        if mask:
+            variables = variables + ['mask_' + v for v in variables]
+
+    # Construct the model
+    model = construct_simple_lstm()
+    if args.checkpoint_file:
+        model.load_weights(os.path.join(checkpoints_dir, args.checkpoint_file))
+
+    # Compile the model
+    model.compile(
+            optimizer=Adam(),
+            loss=SparseCategoricalCrossentropy(),
+            metrics=['accuracy'])
+
+    model.summary()
+
+    if training:
+        train_data = tf.data.Dataset.from_generator(data_generator,
+                args=['data/train_list.txt', steps, batch_size, mask],
+                output_types=(tf.float32, tf.int16),
+                output_shapes=((None, len(variables)), ()))
+
+        val_data = tf.data.Dataset.from_generator(data_generator,
+                args=['data/val_list.txt', steps, batch_size, mask],
+                output_types=(tf.float32, tf.int16),
+                output_shapes=((None, len(variables)), ()))
+
+        # Sort the data set batches by sequence length
+        train_data = train_data.apply(bucket_by_seq_len)
+        val_data = val_data.apply(bucket_by_seq_len)
+
+        # Callbacks
+        checkpoint_callback = ModelCheckpoint(checkpoint_path)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
+                histogram_freq=1)
+        logger_callback = CSVLogger(os.path.join(models_path, 'logs',
+            'logs.csv'))
+        early_stopping_callback = EarlyStopping(monitor='val_loss', min_delta=0,
+                patience=0)
+        callbacks = [early_stopping_callback, checkpoint_callback,
+                logger_callback, tensorboard_callback]
+
+        model.fit(
+            train_data,
+            validation_data=val_data,
+            epochs=args.epochs,
+            initial_epoch=initial_epoch,
+            steps_per_epoch=steps,
+            validation_steps=steps,
+            callbacks=callbacks)
+
+
+if __name__ == '__main__':
+    main(parse_cl_args())
+
