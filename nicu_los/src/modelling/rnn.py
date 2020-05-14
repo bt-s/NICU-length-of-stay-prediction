eacec05c5b53a00d15e242e9f373165047b42a3d
Bas Straathof <bas_straathof@msn.com>
2020-05-14 16:00:23 +0200

    Allow for better GPU memory handling

--- nicu_los/src/modelling/rnn.py
+++ nicu_los/src/modelling/rnn.py
@@ -97,7 +97,12 @@ def parse_cl_args():
 def main(args):
     if args.enable_gpu:
         print('=> Using GPU(s)')
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+        for i, _ in enumerate(physical_devices):
+            config = tf.config.experimental.set_memory_growth(physical_devices[i], True)
         strategy = tf.distribute.MirroredStrategy()
+
     else:
         print('=> Using CPU(s)')
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
