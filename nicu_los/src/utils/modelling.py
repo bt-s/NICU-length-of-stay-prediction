06a9ec3457bd707ff6d6c687f630a5050f9af688
bt-s <bas_straathof@msn.com>
2020-05-14 18:16:30 +0200

    First try at a channel-wise RNN

--- nicu_los/src/utils/modelling.py
+++ nicu_los/src/utils/modelling.py
@@ -14,17 +14,42 @@ import pandas as pd
 
 from tqdm import tqdm
 
+import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Activation, BatchNormalization, \
         Bidirectional, concatenate, Conv1D, Dense, Dropout, \
-        GlobalAveragePooling1D, GRU, Input, LSTM, Masking, multiply, Reshape
+        GlobalAveragePooling1D, GRU, Input, Layer, LSTM, Masking, multiply, \
+        Reshape
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.optimizers import Adam
 
 from nicu_los.src.utils.utils import get_subject_dirs
-from nicu_los.src.utils.readers import TimeSeriesReader 
+from nicu_los.src.utils.readers import TimeSeriesReader
 from nicu_los.src.utils.evaluation import evaluate_classification_model
+
+
+class Slice(Layer):
+    """Take a channel (i.e. feature-mask combination) slice of a 3D tensor"""
+    def __init__(self, feature, mask, **kwargs):
+        self.slice = [feature, mask]
+        super(Slice, self).__init__(**kwargs)
+
+    def call(self, X, mask=None):
+        # Permute the tensor
+        X = tf.transpose(X, perm=(2, 0, 1))
+
+        # Gather the feature and corresponding mask
+        X = tf.gather(X, self.slice)
+
+        # Re-permute the tensor
+        X = tf.transpose(X, perm=(1, 2, 0))
+
+        return X
+
+    # Layer must override get_config
+    def get_config(self):
+        return {'slice': self.slice}
 
 
 def create_list_file(subject_dirs, list_file_path,
@@ -102,8 +127,8 @@ def get_baseline_datasets(subject_dirs, coarse_targets=False,
     Args:
         subject_dirs (list): List of subject directories
         coarse_targets (bool): Whether to use coarse targets
-        pre_imputed (bool): Whether to use features from pre-imputed data 
-        targets_only (bool): Whether to only load the targets 
+        pre_imputed (bool): Whether to use features from pre-imputed data
+        targets_only (bool): Whether to only load the targets
 
     Returns:
         X (np.ndarray|None): Features
@@ -125,7 +150,8 @@ def get_baseline_datasets(subject_dirs, coarse_targets=False,
         variables = ['mask_' + v for v in variables]
 
     if not targets_only:
-        X = np.zeros((tot_num_sub_seqs, len(variables)*len(sub_seqs)*len(stat_fns)))
+        X = np.zeros((tot_num_sub_seqs,
+            len(variables)*len(sub_seqs)*len(stat_fns)))
     else:
         X = None
 
@@ -339,6 +365,9 @@ def construct_and_compile_model(model_type, model_name, checkpoint_file,
     if model_type == 'lstm' or model_type == 'gru':
         model = construct_rnn(input_dimension, output_dimension, model_type,
                 n_cells, dropout, hid_dimension, model_name)
+    elif model_type == 'lstm_cw' or model_type == 'gru_cw':
+        model = construct_channel_wise_rnn(input_dimension, output_dimension,
+                model_type, n_cells, dropout, hid_dimension, model_name)
     elif model_type == 'lstm_fcn':
         model = construct_lstm_fcn(input_dimension, output_dimension, dropout,
                 hid_dimension, False, model_name)
@@ -405,4 +434,82 @@ class MetricsCallback(Callback):
         evaluate_classification_model(np.concatenate(y_true, axis=0),
                 np.concatenate(y_pred, axis=0))
 
+
+    def construct_channel_wise_rnn(input_dimension, output_dimension,
+            model_type='lstm_cw', n_cells=1, dropout=0.0, hid_dimension=16,
+            size_coef=8, model_name=""):
+    """Construct an RNN model (either LSTM or GRU)
+
+    Args:
+        input_dimension (int): Input dimension of the model
+        output_dimension (int): Output dimension of the model
+        n_cells (int): Number of RNN cells
+        dropout (float): Amount of dropout to apply
+        hid_dimension (int): Dimension of the hidden layer (i.e. # of unit in
+                             the RNN cell)
+
+    Returns:
+        model (tf.keras.Model): Constructed RNN model
+    """
+    X = Input(shape=(None, input_dimension))
+    inputs = [X]
+
+    # Skip timestep if all  values of the input tensor are 0
+    X = Masking()(X)
+
+    # Train LSTMs over the channels, and append them
+    cXs = []
+    for feature in range(int(input_dimension/2)):
+        mask = int(feature+input_dimension/2)
+        channel_slice = Slice(feature, mask)(X)
+
+        for layer in range(n_cells):
+            num_hid_units = hid_dimension // 2
+
+            cell = LSTM(units=num_hid_units, activation='tanh',
+                    return_sequences=True, recurrent_dropout=dropout,
+                    dropout=dropout)
+
+            cXs.append(Bidirectional(cell)(channel_slice))
+
+    # Concatenate the channels
+    X = concatenate(cXs, axis=2)
+
+    for layer in range(n_cells - 1):
+        num_hid_units = int(size_coef*hid_dimension) // 2
+
+        if model_type == 'lstm_cw':
+            cell = LSTM(units=num_hid_units, activation='tanh',
+                    return_sequences=True, recurrent_dropout=dropout,
+                    dropout=dropout)
+        elif model_type == 'gru_cw':
+            cell = GRU(units=num_hid_units, activation='tanh',
+                    return_sequences=True, recurrent_dropout=dropout,
+                    dropout=dropout)
+        else:
+            raise ValueError("Parameter 'model_type' should be one of " +
+                    "'lstm_cw' or 'gru_cw'.")
+
+        X = Bidirectional(cell)(Z)
+
+    # There always has to be at least one cell
+    if model_type == 'lstm_cw':
+        X = LSTM(activation='tanh', dropout=dropout, recurrent_dropout=dropout,
+                return_sequences=False, units=hid_dimension)(X)
+    elif model_type == 'gru_cw':
+        X = GRU(activation='tanh', dropout=dropout, recurrent_dropout=dropout,
+                return_sequences=False, units=hid_dimension)(X)
+    else:
+        raise ValueError("Parameter 'model_type' should be one of " +
+                "'lstm_cw' or 'gru_cw'.")
+
+    if dropout:
+        X = Dropout(dropout)(X)
+
+    y = Dense(units=output_dimension, activation='softmax')(X)
+    outputs = [y]
+
+    model = Model(inputs=inputs, outputs=outputs, name=model_name)
+
+    return model
 
