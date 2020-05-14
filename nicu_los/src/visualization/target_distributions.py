007b3daecdb64e733c7546f42d39fc5dc8684599
bt-s <bas_straathof@msn.com>
2020-05-14 16:10:08 +0200

    Update target distribution script

--- nicu_los/src/visualization/target_distributions.py
+++ nicu_los/src/visualization/target_distributions.py
@@ -27,8 +27,6 @@ def parse_cl_args():
             help='Path to subject directories.')
     parser.add_argument('-pp', '--plots-path', type=str, default='plots/',
             help='Path to plots directory.')
-    parser.add_argument('-v', '--verbose', type=int,
-            help='Level of verbosity in console output.', default=1)
 
     return parser.parse_args(argv[1:])
 
@@ -46,8 +44,9 @@ def main(args):
     subject_directories_test = get_subject_dirs(test_path)
     subject_directories = subject_directories_train + subject_directories_test
 
-    los_hours, los_targets = [], []
-    los_remaining_hours, los_remaining_targets = [], []
+    los_hours, los_remaining_hours, los_targets_coarse, \
+            los_remaining_targets_coarse, los_targets_fine, \
+            los_remaining_targets_fine =  [], [], [], [], [], []
 
     for i, sd in enumerate(tqdm(subject_directories)):
         # Read the timeseries dataframe
@@ -56,14 +55,20 @@ def main(args):
         # Find the total length of the stay in hours
         los_hours.append(ts.LOS_HOURS.iloc[0])
 
-        # Compute the target bucket for the complete stay
-        los_targets.append(ts.TARGET.iloc[0])
+        # Compute the coarse target bucket for the complete stay
+        los_targets_coarse.append(ts.TARGET_COARSE.iloc[0])
+
+        # Compute the fine target bucket for the complete stay
+        los_targets_fine.append(ts.TARGET_FINE.iloc[0])
 
         # Find all the intermediate remaining length of stay in hours
         los_remaining_hours += ts.LOS_HOURS.to_list()
 
-        # Obtain the target bucket for each intermediate time-step
-        los_remaining_targets += ts.TARGET.to_list()
+        # Obtain the coarse target bucket for each intermediate time-step
+        los_remaining_targets_coarse += ts.TARGET_COARSE.to_list()
+
+        # Obtain the fine target bucket for each intermediate time-step
+        los_remaining_targets_fine += ts.TARGET_FINE.to_list()
 
     # Only keep the 95% percentile of los_hours and los_remaining_hours
     los_perc = np.percentile(los_hours, 95)
@@ -73,16 +78,28 @@ def main(args):
     los_remaining_hours = list(filter(lambda x: x < los_remaining_perc,
             los_remaining_hours))
 
-    # X-ticks for the buckets plot
-    xticks = ['(0, 1)', '(1, 2)', '(2, 3)', '(3, 4)', '(4, 5)', '(5, 6)',
+    # X-ticks for the coarse buckets plot
+    xticks_coarse = ['(0, 2)', '(2, 8)', '8+']
+
+    # X-ticks for the fine buckets plot
+    xticks_fine = ['(0, 1)', '(1, 2)', '(2, 3)', '(3, 4)', '(4, 5)', '(5, 6)',
             '(6, 7)', '(7, 8)', '(8, 14)', '14+']
 
-    # Create the buckets histogram
-    create_histogram(input_data=[los_targets, los_remaining_targets],
-            xlabel='Buckets', ylabel='Frequency', rwidth=0.5,
-            legend=['LOS', 'Remaining LOS'], xticks=xticks,
+    # Create the coarse buckets histogram
+    create_histogram(input_data=[los_targets_coarse,
+        los_remaining_targets_coarse], xlabel='Buckets',
+        ylabel='Frequency', rwidth=0.5, legend=['LOS (coarse)',
+            'Remaining LOS (coarse)'], xticks=xticks_coarse,
             save_plot=(os.path.join(args.plots_path,
-                'normalized_frequency_of_the_target_buckets')))
+                'normalized_frequency_of_the_target_buckets_coarse')))
+
+    # Create the fine buckets histogram
+    create_histogram(input_data=[los_targets_fine,
+        los_remaining_targets_fine], xlabel='Buckets',
+        ylabel='Frequency', rwidth=0.5, legend=['LOS (fine)',
+            'Remaining LOS (fine)'], xticks=xticks_fine,
+            save_plot=(os.path.join(args.plots_path,
+                'normalized_frequency_of_the_target_buckets_fine')))
 
     # Create the LOS hours histogram
     create_histogram(input_data=[los_hours, los_remaining_hours],
