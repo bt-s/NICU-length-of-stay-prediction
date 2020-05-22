573c3e6f8c6f729e5061f8709074270d682a7cb5
Bas Straathof <bas_straathof@msn.com>
2020-05-23 01:21:45 +0200

    Create script to train a GRU regressor

---
+++ scripts/training_scripts/train_gru_regression.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+mkdir -p logs
+
+# One cell GRU network -- regression
+python -m nicu_los.src.modelling.rnn --regression --model-type gru \
+  --model-name gru_regression_1_cell_dropout_0_3 --n-cells 1 --dropout 0.3 \
+  --batch-size 8 --training-steps 4096 --validation-steps 2048 
+
