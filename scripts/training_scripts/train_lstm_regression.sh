17fce7d6ce4a72c33e640b6c4094dfa136176063
Bas Straathof <bas_straathof@msn.com>
2020-05-23 19:40:22 +0200

    Create script for training an LSTM regressor

---
+++ scripts/training_scripts/train_lstm_regression.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+mkdir -p logs
+
+# One cell LSTM network -- regression
+python -m nicu_los.src.modelling.rnn --regression --model-type lstm \
+  --model-name lstm_regression_1_cell_dropout_0_3 --n-cells 1 --dropout 0.3 \
+  --batch-size 8 --training-steps 4096 --validation-steps 2048 
