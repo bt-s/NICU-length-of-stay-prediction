4bbdda111fa8989d2d9094c8295f2e1de446089f
Bas Straathof <bas_straathof@msn.com>
2020-05-25 01:01:57 +0200

    Update test scripts for logistic and linear regression

--- scripts/test_scripts/test_linear_regression_models.sh
+++ scripts/test_scripts/test_linear_regression_models.sh
@@ -7,6 +7,6 @@ python -m nicu_los.src.modelling.linear_regression --model-name \
   lin_reg_pre_imputed --pre-imputed --testing
 
 # Linear regression not pre-imputed
-#python -m nicu_los.src.modelling.linear_regression --model-name \
-  #lin_reg_non_pre_imputed --not-pre-imputed --training
+python -m nicu_los.src.modelling.linear_regression --model-name \
+  lin_reg_non_pre_imputed --not-pre-imputed --testing
 
