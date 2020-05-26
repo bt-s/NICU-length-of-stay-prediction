d1323d1a762b133b3dfebc1c70cc4e6732f7147f
Bas Straathof <bas_straathof@msn.com>
2020-05-26 23:37:56 +0200

    Update .gitignore and remove test scripts from cache

--- scripts/test_scripts/test_gru_fine_best.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-mkdir -p logs
-
-# Testing the best GRU with fine labels
-python -m nicu_los.src.modelling.rnn --fine-targets --checkpoint-file gru_fine_2_cell_dropout_0_2-batch8-steps2500-epoch04.h5 --n-cells 2 --dropout 0.2 --testing --model-type gru
-
